0aff9ee1dfced13d0cfba28da3b5eb3bcb4cb35f
NaokiHidaka <naoki6855@gmail.com>
2019-02-19 23:05:02 +0900

    事前に構築されたUIを使用するFireBaseUIActivityクラスを追加

--- app/src/main/java/com/example/chef_/firebasesample/FireBaseUIActivity.java
+++ app/src/main/java/com/example/chef_/firebasesample/FireBaseUIActivity.java
@@ -1,10 +1,10 @@
 package com.example.chef_.firebasesample;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
@@ -16,7 +16,7 @@ import com.google.firebase.auth.FirebaseUser;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class FireBaseUIActivity extends AppCompatActivity {
 
     private final static int RC_SIGN_IN = 123;
 
@@ -87,5 +87,4 @@ public class MainActivity extends AppCompatActivity {
                 });
         // [END auth_fui_delete]
     }
-
 }
